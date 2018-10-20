9907d45b65cdf092a5b3a4f41c25ec7d33848fe9
rzhu61 <rayzhu12@gmail.com>
2018-10-20 14:21:32 -0400

    FIREBASE WORKING
    can input a transaction amt and reason, toast notification if not valid

--- app/src/main/java/edu/gatech/split/Controllers/TransactionActivity.java
+++ app/src/main/java/edu/gatech/split/Controllers/TransactionActivity.java
@@ -8,19 +8,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import edu.gatech.split.Model.Transaction;
 import edu.gatech.split.R;
 
 public class TransactionActivity extends AppCompatActivity {
-
+    DatabaseReference databaseTransactions;
+    EditText total;
+    EditText purpose;
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Button cancelButton = (Button) findViewById(R.id.cancel);
 
+        databaseTransactions = FirebaseDatabase.getInstance().getReference("transactions");
+        Button cancelButton = (Button) findViewById(R.id.cancel);
+        submitButton = (Button) findViewById(R.id.submit);
+        total = (EditText) findViewById(R.id.total);
+        purpose = (EditText) findViewById(R.id.purpose);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTransaction();
+            }
+        });
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,7 +51,20 @@ public class TransactionActivity extends AppCompatActivity {
         });
 
     }
-
+    private void addTransaction() {
+        if (total.getText().toString().length()!=0 && purpose.getText().toString().length()!=0) {
+            double amount = Double.parseDouble(total.getText().toString().trim());
+            String subject = purpose.getText().toString().trim();
+            if (amount != 0 && !subject.isEmpty()) {
+                String key = databaseTransactions.push().getKey();
+                Transaction txn = new Transaction(key, subject, amount);
+                databaseTransactions.child(key).setValue(txn);
+                Toast.makeText(this, "Transaction recorded.", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Can't have empty fields.", Toast.LENGTH_LONG).show();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/edu/gatech/split/Model/Transaction.java
+++ app/src/main/java/edu/gatech/split/Model/Transaction.java
@@ -1,8 +1,12 @@
 package edu.gatech.split.Model;
 
 public class Transaction {
+    String key;
+    String purpose;
     double amount;
-    public Transaction(double amount) {
+    public Transaction(String key, String purpose, double amount) {
+        this.key = key;
+        this.purpose = purpose;
         this.amount = amount;
     }
 }
